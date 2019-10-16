332055875a16007ef4a60b61c961a5e38ea497eb
Kevin Wei <chenghaowei@gmail.com>
2019-10-16 11:15:16 -0400

    update error handler

--- src/bin/errorHandler.js
+++ src/bin/errorHandler.js
@@ -5,8 +5,8 @@ function errorHandler(err, req, res, next) {
   logger.error(err.message);
   const response =
     NODE_ENV === 'production'
-      ? { error: { message: 'server error' } }
-      : { message: err.message, error: err };
+      ? { error: 'Internal Server Error' }
+      : { error: err.message, object: err };
   return res.status(err.status ? err.status : 500).json(response);
 }
 
